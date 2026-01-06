6701585e3cac3c1f2d189844e5030acd6c5400a9
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-06 20:30:59 +0600

    feat(models): add fromRssItem factory with HTML cleaning Implemented a factory constructor to parse RSS items directly into ArticleModel.
    Changes:
    - Added  factory.
    - Included  helper to strip HTML tags and decode entities (&nbsp;, &laquo;, etc.).
    - Added fallback logic for categories (defaults to Новости).

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,1 +1,47 @@
-class ArticleModel {}
+class ArticleModel {
+  final String title;
+  final String link;
+  final String description;
+  final String category;
+  final DateTime? pubDate;
+
+  ArticleModel({
+    required this.title,
+    required this.link,
+    required this.description,
+    required this.category,
+    this.pubDate,
+  });
+
+  // ЭТО НОВОЕ: Фабричный конструктор
+  // Зачем: Он берет "грязный" элемент из RSS и делает "чистый" объект ArticleModel
+
+  factory ArticleModel.fromRssItem(item) {
+    // Функция-уборщик: убирает &nbsp; и HTML теги
+
+    String cleanText(String? text) {
+      if (text == null) return ' ';
+
+      return text
+          .replaceAll('&nbsp;', ' ')
+          .replaceAll('&laquo;', '«')
+          .replaceAll('&raquo;', '»')
+          .replaceAll('&mdash;', '—')
+          .replaceAll(
+            RegExp(r'<[^>]*>'),
+            '',
+          ) // Убираем любые теги типа <br>, <p>
+          .trim();
+    }
+
+    return ArticleModel(
+      title: cleanText(item.title),
+      link: item.link ?? '',
+      description: cleanText(item.description),
+      category: item.categories?.isNotEmpty == true
+          ? item.categories!.firs.value
+          : "Новости",
+      pubDate: item.pubDate,
+    );
+  }
+}

aa7b717cf6dc6a9ed5df8f898a079f34f8681001
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-01-07 22:27:17 +0600

    feat(provider): add dynamic category generation and filtering logic Enhanced NewsProvider with category management: - Implemented logic to extract unique categories from fetched articles. - Added custom sorting for categories based on . - Added  getter with filtering logic based on selection. - Updated  to populate categories after loading.

--- lib/providers/news_provider.dart
+++ lib/providers/news_provider.dart
@@ -5,20 +5,43 @@ import 'package:news24_kg/services/rss_service.dart';
 class NewsProvider extends ChangeNotifier {
   final RssService _rssService = RssService();
 
-  // state
+  // state for UI
 
-  List<ArticleModel> _articles = [];
+  List<ArticleModel> _allArticles = [];
+  List<String> _categories = ['Все'];
+  String _selectedCategories = 'Все';
+  final List<String> _categoryOrder = [
+    'Все',
+    'Власть',
+    'Экономика',
+    'Общество',
+    'Проишествия',
+    'Мир',
+    'Техноблог',
+  ];
+
   bool _isLoading = false;
   String _errorMessage = '';
 
-  // getter for UI
+  // getter
 
-  List<ArticleModel> get articles => _articles;
+  List<ArticleModel> get allArticles => _allArticles;
 
   bool get isLoading => _isLoading;
 
   String get errorMessage => _errorMessage;
 
+  RssService get rssService => _rssService;
+
+  List<String> get categories => _categories;
+
+  String get selectedCategories => _selectedCategories;
+
+  List<String> get categoryOrder => _categoryOrder;
+
+  // methods
+
+  /// метод загрузки новостей
   Future<void> fetchNews() async {
     _isLoading = true;
 
@@ -27,12 +50,47 @@ class NewsProvider extends ChangeNotifier {
     notifyListeners();
 
     try {
-      _articles = await _rssService.getFeed();
+      _allArticles = await _rssService.getFeed();
+
+      // 1. Получаем уникальные категории из новостей
+      final uniqueCategories = _allArticles
+          .map((e) => e.category)
+          .where((c) => c.isNotEmpty) // Убираем пустые, если есть
+          .toSet()
+          .toList();
+
+      // 2. Сортируем их согласно нашему списку _categoryOrder
+      uniqueCategories.sort((a, b) {
+        int indexA = _categoryOrder.indexOf(a);
+        int indexB = _categoryOrder.indexOf(b);
+
+        // Если категории нет в нашем списке приоритетов, кидаем её в конец
+        if (indexA == -1) indexA = 999;
+        if (indexB == -1) indexB = 999;
+
+        return indexA.compareTo(indexB);
+      });
+
+      // 3. Формируем итоговый список (Все + отсортированные)
+      _categories = ['Все', ...uniqueCategories];
     } catch (e) {
       _errorMessage = e.toString();
     } finally {
       _isLoading = false;
       notifyListeners();
     }
+  }
+
+  /// логика фильтраций
+  /// Если выбрано "Все" -> отдаем весь список.
+  /// Если выбрано другая категория -> отдаем только статьи с определенными новостями.
+
+  List<ArticleModel> get articles {
+    if (_selectedCategories == 'Все') {
+      return _allArticles;
+    }
+    return _allArticles
+        .where((article) => article.category == _selectedCategories)
+        .toList();
   }
 }
